9eebfdad1e18b36c487fddf24201b62d4628bb61
John Lovitsch <jmlovitsch@gmail.com>
2020-09-02 14:35:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,4 +1,5 @@
 def addition(num1, num2)
+<<<<<<< HEAD
 sum = num1 + num2
 sum
 end
@@ -33,3 +34,40 @@ end
    tyt
  end
  square_root(81)
+=======
+  num1 + num2
+  addition(5,4)
+end
+addition(5,4)
+
+def subtraction(num1, num2)
+  sum = num1 - num2
+  sum
+end
+subtraction(10,5)
+
+def division(num1, num2)
+  sum = num1 / num2
+  sum
+end
+division(50,2)
+
+def multiplication(num1, num2)
+ sum = num1 * num2
+ sum
+end
+multiplication(4,30)
+
+def modulo(num1, num2)
+ sum = num1 % num2
+ sum
+end
+modulo(34, 5)
+
+def square_root(num)
+sum = math.sqrt(num)
+sum
+end
+square_root(81)
+
+>>>>>>> c748479382d12368ffb3efe9f4941d54d7eb5165
